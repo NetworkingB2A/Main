81d40b281aaa8e7770a642c0f20f77b612b211f7
Adam Angell <Adam.Angell@networkingb2a.com>
2023-05-19 17:57:27 +0000

    Cloud Notes

---
+++ Training/Cloud/Azure/terraform training/terraform.tfvars
@@ -0,0 +1,4 @@
+subscripbtionID = ""
+location = "eastus"
+resourceGroupName = "adam_lab_terraform_rg"
+vnet_name = "adam_lab_terraform_vnet"
---
+++ Training/Cloud/Azure/terraform training/variables.tf
@@ -0,0 +1,23 @@
+variable "subscripbtionID" {
+    type = string
+    description = "name of my sub id"
+}
+
+variable "location" {
+    type = string
+    description = "the location of the region"
+}
+
+variable "resourceGroupName" {
+    type = string
+    description = "resource group name"
+  
+}
+
+variable "vnet_name" {
+    type = string
+    description = "vnet name"
+  
+}
+
+
